7644fed8c2bfcd21658e27b7caea69096df32431
atomicbrixton <atomic.brixton@gmail.com>
2016-05-25 20:11:10 -0600

    #TOOBIG - Why?

--- Source/AtomicCatalog/AtomicCatalog.WebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AtomicCatalog.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Source/AtomicCatalog/AtomicCatalog.WebApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AtomicCatalog.WebApi.WebApiApplication" Language="C#" %>
