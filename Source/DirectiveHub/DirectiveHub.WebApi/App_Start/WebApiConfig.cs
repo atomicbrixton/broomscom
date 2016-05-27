fbcbba58805af23acb9c30c08d059eb81004e6d0
atomicbrixton <atomic.brixton@gmail.com>
2016-05-26 19:24:50 -0600

    #SETUP - Huh

--- Source/DirectiveHub/DirectiveHub.WebApi/App_Start/WebApiConfig.cs
+++ Source/DirectiveHub/DirectiveHub.WebApi/App_Start/WebApiConfig.cs
@@ -9,16 +9,8 @@ namespace DirectiveHub.WebApi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
+            // Map
             config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 }
